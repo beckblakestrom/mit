64ec43379f65eaf300123f0ba04545de42d86d8c
Blake Beckstrom <dabeck88@gmail.com>
2022-01-16 21:57:36 -0500

    rendered react on dom

--- week/12/modules/jsx/element.jsx
+++ week/12/modules/jsx/element.jsx
@@ -2,3 +2,10 @@ const name = "Blake";
 const element = <h4> Whats Up {name} </h4>;
 
 ReactDOM.render(element, document.getElementById("root"));
+
+const Hello = (props) => {
+	const title = props.title;
+	return <h2> Yo diggity {title}</h2>;
+};
+
+ReactDOM.render(<Hello title="Frank" />, document.getElementById("base"));
