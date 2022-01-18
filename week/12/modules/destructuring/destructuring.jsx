b73a3429018abc7ac4557d453a804f368e866597
Blake Beckstrom <dabeck88@gmail.com>
2022-01-17 21:41:39 -0500

    added some destructuring

---
+++ week/12/modules/destructuring/destructuring.jsx
@@ -0,0 +1,31 @@
+const App = () => {
+	const handler = () => alert(`Yoda Bruh`);
+	return <MyButton onClick={handler}></MyButton>;
+};
+
+// const MyButton = (props) => {
+// 	return <button onClick={props.onClick}>Star Wars?</button>;
+// };
+
+// const MyButton = ({ onClick }) => {
+// 	return <button onClick={onClick}>Star Wars?</button>;
+// };
+
+import Button from "react-bootstrap/Button";
+
+const MyButton = ({ onClick }) => {
+	let { Button } = ReactBootstrap;
+	return <Button onClick={onClick}>Star Wars?</Button>;
+};
+
+const FruitStand = () => {
+	let product = { name: "pear", cost: 2, inStock: 7 };
+	let { name, inStock } = product;
+	let item = { name, inStock };
+
+	const handler = () => alert(`name: ${item.name} instock: ${item.inStock}`);
+	return <button onClick={handler}>Fruit?</button>;
+};
+
+ReactDOM.render(<App />, document.getElementById("container"));
+ReactDOM.render(<FruitStand />, document.getElementById("fruit__container"));
--- week/12/readme.md
+++ week/12/readme.md
@@ -1,1 +1,3 @@
 # week 12: Introduction to React
+
+can't figure out react bootstrap
