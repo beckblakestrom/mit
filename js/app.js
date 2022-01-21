05cf186afc6885cb21f28fbbd6ccf0a02f490796
Blake Beckstrom <dabeck88@gmail.com>
2022-01-20 19:24:08 -0500

    added initial category container opening

--- js/app.js
+++ js/app.js
@@ -6,6 +6,18 @@ menuButton.onclick = () => {
 	menuButtonBackground.classList.toggle("open-menu__button-background");
 };
 
-function openContainer(container) {
-	console.log(container);
-}
+// function openContainer(container) {
+// 	let parent = document.getElementById(container).parentNode;
+// 	console.log(parent);
+// 	parent.classList.toggle("open-container");
+// 	console.log(`${parent.className} height increased to 50%`);
+// 	let opener = document.getElementById(container).nextElementSibling;
+// 	opener.classList.toggle("open-link");
+// }
+
+menuBrowserWeek.onclick = () => {
+	let parent = menuBrowserWeek.parentNode;
+	parent.classList.toggle("open-container");
+	let opener = menuBrowserWeek.nextElementSibling;
+	opener.classList.toggle("open-link");
+};
