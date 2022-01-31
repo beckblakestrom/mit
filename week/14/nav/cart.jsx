f9cd6d761a7e1641b532883701efd92137bd163d
Blake Beckstrom <dabeck88@gmail.com>
2022-01-31 11:37:57 -0500

    updated cart jsx

--- week/14/nav/cart.jsx
+++ week/14/nav/cart.jsx
@@ -2,17 +2,21 @@ function NavBar({ stockitems }) {
 	const [cart, setCart] = React.useState([]);
 	const [stock, setStock] = React.useState(stockitems);
 
-	const moveToCart = (e) => {
+	const moveToCart = (id, e) => {
 		let [name, num] = e.target.innerHTML.split(":");
 		let newStock = stock.map((item, index) => {
 			if (item.name == name) item.instock++;
 			return item;
 		});
 		setStock(newStock);
+		console.log(id);
 	};
 	const updatedList = stock.map((item, index) => {
 		return (
-			<button className="button-white" onClick={moveToCart} key={index}>
+			<button
+				className="button-white"
+				onClick={(e) => moveToCart({ itemID: item.id }, e)}
+				key={index}>
 				{item.name}:{item.instock}
 			</button>
 		);
@@ -33,11 +37,11 @@ function NavBar({ stockitems }) {
 	);
 }
 const menuItems = [
-	{ name: "apple", instock: 2 },
-	{ name: "pineapple", instock: 3 },
-	{ name: "pear", instock: 0 },
-	{ name: "peach", instock: 3 },
-	{ name: "orange", instock: 1 },
+	{ name: "apple", instock: 2, id: 25668 },
+	{ name: "pineapple", instock: 3, id: 35777 },
+	{ name: "pear", instock: 0, id: 11290 },
+	{ name: "peach", instock: 3, id: 11264 },
+	{ name: "orange", instock: 1, id: 33255 },
 ];
 ReactDOM.render(
 	<NavBar stockitems={menuItems} />,
