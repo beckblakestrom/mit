4d389073558901b9711f0ad06b73d50306f3fc8e
Blake Beckstrom <dabeck88@gmail.com>
2022-02-14 13:16:24 -0500

    added board row and squares

--- week/16/tictactoe/app.js
+++ week/16/tictactoe/app.js
@@ -1,7 +1,14 @@
+const Square = () => {
+	return <button className="square"></button>;
+};
+
 const Board = () => {
-	const myPlayers = [1, 2];
+	const myPlayers = ["One", "Two"];
 	const [player, setPlayer] = React.useState(0);
 	let status = `Player ${myPlayers[player]}`;
+	function renderSquare(i) {
+		return <Square></Square>;
+	}
 
 	function changeClass(e) {
 		let bgg = e.target;
@@ -16,8 +23,13 @@ const Board = () => {
 					changeClass(e);
 				}}
 				className="game-board">
+				<div className="grid-row">
+					{renderSquare(0)}
+					{renderSquare(1)}
+					{renderSquare(2)}
+				</div>
 				<div id="info">
-					<h1> {status} </h1>
+					<h1 className="player"> {status} </h1>
 				</div>
 			</div>
 		</React.Fragment>
