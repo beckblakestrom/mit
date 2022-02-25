e8109da278b3de1449037d9a5aed1a58b6810d40
Blake Beckstrom <dabeck88@gmail.com>
2022-02-24 22:28:08 -0500

    fixed books json file

--- week/17/books/books_main.js
+++ week/17/books/books_main.js
@@ -1,10 +1,29 @@
-class book extends HTMLElement {
+class Book extends HTMLElement {
 	constructor() {
 		super();
+	}
 
-		const title = this.getAttribute("title");
-		const author = this.getAttribute("author");
-		const publisher = this.getAttribute("publisher");
-		const description = this.getAttribute("description");
+	set book(book) {
+		this.innerHTML = `
+        <div class="book">
+				<img
+					src="${book.source}"
+					alt="book image"
+					class="book-img"
+				/>
+				<h1 class="book-title">${book.title}</h1>
+				<div class="book-author">
+					<span class="book-author-by">by</span>
+					<a href="#" class="book-author-name">${book.author}</a>
+				</div>
+				<a href="#" class="book-genre">${book.genre}</a>
+				<p class="book-description">
+				${book.description}
+				</p>
+				<a href="#" class="book-price">${book.price}</a>
+			</div>
+        `;
 	}
 }
+
+customElements.define("new-book", Book);
