3526471f78b21828f1a0821da07abeda3d8e97c9
Blake Beckstrom <dabeck88@gmail.com>
2022-02-25 20:46:10 -0500

    turned book catalog into react component

--- week/17/books/books_main.js
+++ week/17/books/books_main.js
@@ -1,29 +1,23 @@
-class Book extends HTMLElement {
-	constructor() {
-		super();
-	}
-
-	set book(book) {
-		this.innerHTML = `
-        <div class="book">
-				<img
-					src="${book.source}"
-					alt="book image"
-					class="book-img"
-				/>
-				<h1 class="book-title">${book.title}</h1>
-				<div class="book-author">
-					<span class="book-author-by">by</span>
-					<a href="#" class="book-author-name">${book.author}</a>
-				</div>
-				<a href="#" class="book-genre">${book.genre}</a>
-				<p class="book-description">
-				${book.description}
-				</p>
-				<a href="#" class="book-price">${book.price}</a>
+function Book({ data }) {
+	return (
+		<div className="book">
+			<img src={data.source} alt="book image" className="book-img" />
+			<h1 className="book-title">{data.title}</h1>
+			<div className="book-author">
+				<span className="book-author-by">by</span>
+				<a href="#" className="book-author-name">
+					{data.author}
+				</a>
 			</div>
-        `;
-	}
+			<a href="#" className="book-genre">
+				{data.genre}
+			</a>
+			<p className="book-description">{data.description}</p>
+			<a href="#" className="book-price">
+				{data.price}
+			</a>
+		</div>
+	);
 }
 
 customElements.define("new-book", Book);
