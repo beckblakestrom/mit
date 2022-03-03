c57a65ce465e80d307471bd7a5242cbfd5393637
Blake Beckstrom <dabeck88@gmail.com>
2022-03-03 15:26:19 -0500

    added home link

--- week/18/index.js
+++ week/18/index.js
@@ -7,6 +7,7 @@ function Spa() {
 		<HashRouter>
 			<div>
 				<h1>Routing -- Hello World</h1>
+				<Link to="/">Home</Link>
 				<Link to="/about/">About</Link>
 				<Link to="/products/">Products</Link>
 				<hr></hr>
