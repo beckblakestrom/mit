f70c7e8c72781dd6112c1aa94d07d22ad55b8fa6
Blake Beckstrom <dabeck88@gmail.com>
2022-03-09 12:38:19 -0500

    updated create account

--- week/18/js/createaccount.js
+++ week/18/js/createaccount.js
@@ -31,127 +31,126 @@ function CreateAccount() {
 		setPassword("");
 		setShow(true);
 	}
-	return (
-		<Card
-			header="Create Account"
-			status={status}
-			body={
-				show ? (
-					<React.Fragment>
-						<div className="form_container">
-							<form className="form">
-								<div className="form_section">
-									<input
-										type="text"
-										name="firstName"
-										id="firstName"
-										className="form_input"
-										placeholder="First Name"
-										value={firstName}
-										onChange={(e) => setFirstName(e.currentTarget.value)}
-										required
-									/>
-									<label htmlFor="name" className="form_label">
-										First Name
-									</label>
-									<div className="form_input-validator"></div>
-									<svg
-										className="form_check"
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
-									</svg>
-								</div>
-								<div className="form_section">
-									<input
-										type="text"
-										name="lastname"
-										id="lastname"
-										className="form_input"
-										placeholder="Last Name"
-										value={lastName}
-										onChange={(e) => setLastName(e.currentTarget.value)}
-										required
-									/>
-									<label htmlFor="lastName" className="form_label">
-										Last Name
-									</label>
-									<div className="form_input-validator"></div>
-									<svg
-										className="form_check"
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
-									</svg>
-								</div>
-								<div className="form_section">
-									<input
-										type="email"
-										name="email"
-										id="email"
-										className="form_input form_input-email"
-										placeholder="Email Address"
-										required
-										autoComplete="email"
-										value={email}
-										onChange={(e) => setEmail(e.currentTarget.value)}
-									/>
-									<label htmlFor="email" className="form_label">
-										Email Address
-									</label>
-									<div className="form_input-validator"></div>
-									<svg
-										className="form_check"
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
-									</svg>
-								</div>
-								<div className="form_section">
-									<input
-										type="password"
-										name="password"
-										id="password"
-										className="form_input"
-										placeholder="Password"
-										value={password}
-										onChange={(e) => setPassword(e.currentTarget.value)}
-										required
-									/>
-									<label htmlFor="password" className="form_label">
-										Password
-									</label>
-									<div className="form_input-validator"></div>
-									<svg
-										className="form_check"
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
-									</svg>
-								</div>
-								<button onClick={handleCreate} className="submit" type="submit">
-									Create Account
-								</button>
-							</form>
-						</div>
-					</React.Fragment>
-				) : (
-					<React.Fragment>
-						<h5>Success</h5>
-						<button onClick={clearForm} className="submit" type="submit">
-							Add Another Account
-						</button>
-					</React.Fragment>
-				)
-			}
-		/>
+	return show ? (
+		<div className="full-page-container">
+			<div className="form_container">
+				<form className="form">
+					<h1>Create Account</h1>
+					<div className="form_section">
+						<input
+							type="text"
+							name="firstName"
+							id="firstName"
+							className="form_input"
+							placeholder="First Name"
+							value={firstName}
+							onChange={(e) => setFirstName(e.currentTarget.value)}
+							required
+						/>
+						<label htmlFor="name" className="form_label">
+							First Name
+						</label>
+						<div className="form_input-validator"></div>
+						<svg
+							className="form_check"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24">
+							<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
+						</svg>
+					</div>
+					<div className="form_section">
+						<input
+							type="text"
+							name="lastname"
+							id="lastname"
+							className="form_input"
+							placeholder="Last Name"
+							value={lastName}
+							onChange={(e) => setLastName(e.currentTarget.value)}
+							required
+						/>
+						<label htmlFor="lastName" className="form_label">
+							Last Name
+						</label>
+						<div className="form_input-validator"></div>
+						<svg
+							className="form_check"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24">
+							<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
+						</svg>
+					</div>
+					<div className="form_section">
+						<input
+							type="email"
+							name="email"
+							id="email"
+							className="form_input form_input-email"
+							placeholder="Email Address"
+							required
+							autoComplete="email"
+							value={email}
+							onChange={(e) => setEmail(e.currentTarget.value)}
+						/>
+						<label htmlFor="email" className="form_label">
+							Email Address
+						</label>
+						<div className="form_input-validator"></div>
+						<svg
+							className="form_check"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24">
+							<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
+						</svg>
+					</div>
+					<div className="form_section">
+						<input
+							type="password"
+							name="password"
+							id="password"
+							className="form_input"
+							placeholder="Password"
+							value={password}
+							onChange={(e) => setPassword(e.currentTarget.value)}
+							required
+						/>
+						<label htmlFor="password" className="form_label">
+							Password
+						</label>
+						<div className="form_input-validator"></div>
+						<svg
+							className="form_check"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24">
+							<path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z" />
+						</svg>
+					</div>
+					<button onClick={handleCreate} className="submit" type="submit">
+						Create Account
+					</button>
+					<div className="need-create">
+						<h1 className="need-create-title">Already have an account?</h1>
+						<Link className="need-create-link" to="/login/">
+							Login
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	) : (
+		<div className="full-page-container">
+			<h5>Success</h5>
+			<button onClick={clearForm} className="submit" type="submit">
+				Add Another Account
+			</button>
+		</div>
 	);
 }
