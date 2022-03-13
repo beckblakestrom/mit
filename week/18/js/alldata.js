a5e5ff91a9db6d1798497f1a0cf316dc1cdb1cd0
Blake Beckstrom <dabeck88@gmail.com>
2022-03-12 22:42:47 -0500

    swapped ctx for {user, setUser}

--- week/18/js/alldata.js
+++ week/18/js/alldata.js
@@ -1,14 +1,14 @@
 function AllData() {
-	const ctx = React.useContext(UserContext);
+	const { user, setUser } = React.useContext(UserContext);
 	return (
 		<div>
 			<h1>First Name User 0</h1>
-			{ctx.users[0].firstName}
+			{user.users[0].firstName}
 			<br />
 
 			<div>
 				<h1>All Users</h1>
-				{JSON.stringify(ctx.users)};
+				{JSON.stringify(user.users)};
 			</div>
 		</div>
 	);
