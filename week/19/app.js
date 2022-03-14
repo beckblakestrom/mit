baaaa4925b0e15618925fe64d81c741356380eb1
Blake Beckstrom <dabeck88@gmail.com>
2022-03-13 22:29:20 -0400

    turned products into items object

--- week/19/app.js
+++ week/19/app.js
@@ -1,24 +1,30 @@
 const UserContext = React.createContext(null);
 
 function App() {
-	const [products, setProducts] = React.useState([
-		{
-			item: "Apple",
-			inventory: 5,
-		},
-		{
-			item: "Orange",
-			inventory: 5,
-		},
-		{
-			item: "Pear",
-			inventory: 5,
-		},
-		{
-			item: "Banana",
-			inventory: 5,
-		},
-	]);
+	const [products, setProducts] = React.useState({
+		items: [
+			{
+				item: "Apple",
+				inventory: 5,
+				src: "./img/apple.png",
+			},
+			{
+				item: "Orange",
+				inventory: 5,
+				src: "./img/apple.png",
+			},
+			{
+				item: "Pear",
+				inventory: 5,
+				src: "./img/apple.png",
+			},
+			{
+				item: "Banana",
+				inventory: 5,
+				src: "./img/apple.png",
+			},
+		],
+	});
 	return (
 		<UserContext.Provider value={{ products, setProducts }}>
 			<div className="page-container">
