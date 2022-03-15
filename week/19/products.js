fb1be57a36e60b1144d86c443d79692e495bef59
Blake Beckstrom <dabeck88@gmail.com>
2022-03-14 20:46:11 -0400

    fully working products

--- week/19/products.js
+++ week/19/products.js
@@ -3,25 +3,34 @@ const Products = () => {
 
 	function Listing() {
 		return products.items.map((current) => {
-			console.log(current);
 			let i = products.items.indexOf(current);
 			let src = "./img/" + current.item + ".png";
+			let stock = current.inventory;
+			let out = false;
+			if (stock === 0) {
+				out = true;
+			}
+
 			return (
 				<div key={current.item} className="products-container">
 					<img className="products-img" src={src} />
 					<h1 className="products-item">{current.item}</h1>
 					<h1 className="products-inventory">Inventory: {current.inventory}</h1>
-					<button
-						className="products-button"
-						onClick={() => {
-							let items = [...products.items];
-							let item = { ...items[i] };
-							item.inventory -= 1;
-							items[i] = item;
-							setProducts({ items });
-						}}>
-						Add to Cart
-					</button>
+					{!out ? (
+						<button
+							className="products-button"
+							onClick={() => {
+								let items = [...products.items];
+								let item = { ...items[i] };
+								item.inventory -= 1;
+								items[i] = item;
+								setProducts({ items });
+							}}>
+							Add to Cart
+						</button>
+					) : (
+						<h1 className="products-out">Out of Stock</h1>
+					)}
 				</div>
 			);
 		});
