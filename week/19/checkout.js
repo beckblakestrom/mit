c9129cfbf6c789e35bc6fbf3f536943a100007a7
Blake Beckstrom <dabeck88@gmail.com>
2022-03-16 21:17:20 -0400

    added running total

--- week/19/checkout.js
+++ week/19/checkout.js
@@ -1,5 +1,25 @@
 const Checkout = () => {
 	const { products, setProducts } = React.useContext(UserContext);
+
+	function Total() {
+		let runningTotal = 0;
+		let tab = false;
+		if (runningTotal > 0) {
+			tab = true;
+		}
+
+		products.items.map((current) => {
+			let currentTotal = current.price * current.clicks;
+			runningTotal += currentTotal;
+		});
+		return (
+			<div>
+				<h1>Total</h1>
+				<h1 className="checkout-total">${runningTotal}</h1>
+			</div>
+		);
+	}
+
 	function Basket() {
 		return products.items.map((current) => {
 			let i = products.items.indexOf(current);
@@ -9,12 +29,24 @@ const Checkout = () => {
 			if (basket > 0) {
 				empty = true;
 			}
+			let two = false;
+			if (basket > 1) {
+				two = true;
+			}
 
 			return empty ? (
 				<div key={current.item} className="products-container">
 					<img className="products-img" src={src} />
-					<h1 className="products-item">{current.item}</h1>
-					<h1 className="products-inventory">Basket: {current.clicks}</h1>
+					<div className="products-span">
+						<h1 className="products-item">{current.clicks}</h1>
+						<h1 className="products-item">
+							{!two ? current.item : current.item + "s"}
+						</h1>
+					</div>
+
+					<h1 className="products-inventory">
+						Total: ${current.price * current.clicks}
+					</h1>
 				</div>
 			) : (
 				<React.Fragment></React.Fragment>
@@ -25,6 +57,7 @@ const Checkout = () => {
 	return (
 		<div className="component-container">
 			<Basket />
+			<Total />
 		</div>
 	);
 };
