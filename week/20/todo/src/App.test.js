d59ba807048df66dbf8fa2c641be54210bb338ea
Blake Beckstrom <dabeck88@gmail.com>
2022-03-21 20:33:45 -0400

    added tests

--- week/20/todo/src/App.test.js
+++ week/20/todo/src/App.test.js
@@ -1,16 +1,47 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/no-node-access */
-import { render, screen } from "@testing-library/react";
+import {
+	render,
+	fireEvent,
+	getByPlaceholderText,
+	screen,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 import React from "react";
-import * as ReactDOM from "react-dom";
 
 test("ToDo", () => {
-	const root = document.createElement("div");
-	ReactDOM.render(<App />, root);
+	const { getByText } = render(<App />);
 
 	//after render
 	// use DOM API (query selector) to make assertions
 	// eslint-disable-next-line testing-library/no-node-access
-	expect(root.querySelector("h1").textContent).toBe("My To-Do List");
-	expect(root.querySelector("button").textContent).toBe("Add Task");
+	getByText("My To-Do List");
+	getByText("Add Task");
+});
+
+test("add items to list", () => {
+	const { getByText } = render(<App />);
+
+	//after render
+	// use DOM API (query selector) to make assertions
+	// eslint-disable-next-line testing-library/no-node-access
+	getByText("My To-Do List");
+	const input = screen.getByPlaceholderText("I need to...");
+	fireEvent.change(input, { target: { value: "wash car" } });
+	fireEvent.click(getByText("Add Task"));
+
+	// confirm
+	getByText("wash car");
+});
+// user event expresses intent better
+test("user-events allows users to add...", () => {
+	const { getByText, getByPlaceholderText } = render(<App />);
+	const input = screen.getByPlaceholderText("I need to...");
+	const button = getByText("Add Task");
+
+	userEvent.type(input, "Learn Spanish");
+	userEvent.click(button);
+
+	getByText("Learn Spanish");
 });
