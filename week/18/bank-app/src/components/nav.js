cb596a3c30c22b28de7a39508df51918569d7755
Blake Beckstrom <dabeck88@gmail.com>
2022-03-24 22:22:10 -0400

    added different view for loggedin nav

--- week/18/bank-app/src/components/nav.js
+++ week/18/bank-app/src/components/nav.js
@@ -1,30 +1,74 @@
-import { Link } from "./context";
+import Login from "./login";
+import React from "react";
+import { useState, useContext } from "react";
+import { UserContext, Link } from "./context";
 
 export default function Nav() {
-	return (
-		<div className="nav">
-			<Link to="/" className="logo">
-				Bad Bank
-			</Link>
-			<div className="link-container">
-				<Link className="link" to="/alldata/">
-					Data
+	const { user, setUser, loggedIn, setLoggedIn } = useContext(UserContext);
+
+	return !loggedIn ? (
+		<React.Fragment>
+			<div className="nav">
+				<Link to="/" className="logo">
+					Bad Bank
 				</Link>
-				<Link className="link" to="/balance/">
-					Balance
-				</Link>
-				<Link className="link" to="/deposit/">
-					Deposit
-				</Link>
-				<Link className="link" to="/withdraw/">
-					Withdraw
-				</Link>
+				<div className="link-container">
+					<Link className="link" to="/alldata/">
+						Data
+					</Link>
+					<Link className="link" to="/balance/">
+						Balance
+					</Link>
+					<Link className="link" to="/deposit/">
+						Deposit
+					</Link>
+					<Link className="link" to="/withdraw/">
+						Withdraw
+					</Link>
+				</div>
 			</div>
-			<div className="secondary-nav">
-				<Link className="link" to="/login/">
+			<div id="login-dropdown" className="secondary-nav">
+				<Login />
+				<button
+					className="login-dropdown"
+					onClick={() => {
+						document.getElementById("login-dropdown").classList.toggle("drop");
+					}}>
 					Login
-				</Link>
+				</button>
 			</div>
-		</div>
+		</React.Fragment>
+	) : (
+		<React.Fragment>
+			<div className="nav">
+				<Link to="/" className="logo">
+					Bad Bank
+				</Link>
+				<div className="link-container">
+					<Link className="link" to="/alldata/">
+						Data
+					</Link>
+					<Link className="link" to="/balance/">
+						Balance
+					</Link>
+					<Link className="link" to="/deposit/">
+						Deposit
+					</Link>
+					<Link className="link" to="/withdraw/">
+						Withdraw
+					</Link>
+				</div>
+			</div>
+			<div id="login-dropdown" className="secondary-nav">
+				<Login />
+				<button
+					className="login-dropdown"
+					onClick={() => {
+						setLoggedIn(false);
+					}}>
+					Logout
+				</button>
+			</div>
+		</React.Fragment>
 	);
 }
