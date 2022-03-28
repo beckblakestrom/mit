dad95d44e0f1ae2baf1102d4a99172c37d3e5c49
Blake Beckstrom <dabeck88@gmail.com>
2022-03-28 16:26:14 -0400

    changed banner txt

--- week/18/bank-app/src/components/home.js
+++ week/18/bank-app/src/components/home.js
@@ -4,7 +4,7 @@ import { Banner } from "./context";
 export default function Home() {
 	return (
 		<React.Fragment>
-			<Banner image="/images/banner.jpg" text="The Standard of Excellence" />
+			<Banner image="/images/banner.jpg" text="Banking Redefined" />
 		</React.Fragment>
 	);
 }
